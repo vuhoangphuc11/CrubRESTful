e35a97289aa63b2107feccceb01aa67d852bdaa4
vuhoangphuc11 <vuhoangphuc11@gmail.com>
2021-10-05 16:52:09 +0700

    example

---
+++ src/main/java/com/phucvh/CrubResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.phucvh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrubResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrubResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phucvh/CrubResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucvh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrubResTfulApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
